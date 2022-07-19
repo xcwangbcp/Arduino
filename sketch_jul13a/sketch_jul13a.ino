16cddba22534764ca6c62733276b43e7ead92571
intfreedom <liuwenjunxjtu@163.com>
2022-07-19 13:36:20 +0800

    Update sketch_jul13a.ino

--- sketch_jul13a/sketch_jul13a.ino
+++ sketch_jul13a/sketch_jul13a.ino
@@ -1,3 +1,21 @@
+#include <ArduinoRobot.h>
+#include <Arduino_LCD.h>
+#include <Compass.h>
+#include <EasyTransfer2.h>
+#include <EEPROM_I2C.h>
+#include <Fat16.h>
+#include <Fat16Config.h>
+#include <Fat16mainpage.h>
+#include <Fat16util.h>
+#include <FatStructs.h>
+#include <Multiplexer.h>
+#include <SdCard.h>
+#include <SdInfo.h>
+#include <Squawk.h>
+#include <SquawkSD.h>
+
+#include <Stepper.h>
+
 
 /*
  Stepper Motor Control - one step at a time
