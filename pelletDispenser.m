45066561f46dfb17d9e97171170f042cf97ddc8c
April <xcwang@ion.ac.cn>
2022-09-30 15:35:46 +0800

    a

--- pelletDispenser.m
+++ pelletDispenser.m
@@ -1,59 +1,56 @@
-% clear 
-% delete(instrfind({'Port'},{'COM8'}))
-% a           = arduino('com8','uno','libraries','I2C');
+clear 
+delete(instrfind({'Port'},{'COM8'}))
+a  = arduino('com8','uno','libraries','I2C');
+a.pinMode(8,'output');
+a.pinMode(9,'output');
+a.pinMode(12,'output');
+a.pinMode(13,'output');
+a.pinMode(3,'output');
+a.pinMode(11,'output');
+% a.pinMode(5,'input');
+if monkeytouched
+    driveMotor(a)
+end
 
-global delaylength
-delaylength = 0.01;
-nbstep      = 8;
-% channelA    = 12; % -- HIGH = forwards and LOW = backwards
-% channelB    = 13; % -- HIGH = forwards and LOW = backwards
-% disChannelA = 9;  % //brake (disable) CH A
-% disChannelB = 8;  % //brake (disable) CH B
-% sensorChan  = 5;
-% sensor=readDigitalPin(a,'D5');
-% while 
-% Check for the sensor input
-  for i=1:nbstep
- 
+
+
+function driveMotor(a)
+ delaylength = 0.01;
+ nbstep      = 8;
+ for i      = 1:nbstep
+  check_sensor(a);
   writeDigitalPin(a,'D9', 0);   %//ENABLE CH A
   writeDigitalPin(a,'D8', 1);   %//DISABLE CH B
   writeDigitalPin(a,'D12',1);   %//Sets direction of CH A
   writePWMVoltage(a,'D3', 5);    %//Moves CH A
   pause(delaylength);
-  
-  
+   
+  check_sensor(a);
   writeDigitalPin(a,'D9', 1);   %//DISABLE CH A
   writeDigitalPin(a,'D8', 0);   %//ENABLE CH B
-  writeDigitalPin(a,'D13', 0); %//Sets direction of CH B
-  writePWMVoltage(a,'D11', 5);   %//Moves CH B
+  writeDigitalPin(a,'D13',0);  %//Sets direction of CH B
+  writePWMVoltage(a,'D11',5);   %//Moves CH B
   pause(delaylength);
   
-  sensor=readDigitalPin(a,'D5');
-  if sensor ==0
-       break;
-  end
+  check_sensor(a);
   writeDigitalPin(a,'D9', 0); %//ENABLE CH A
   writeDigitalPin(a,'D8', 1); %//DISABLE CH B
   writeDigitalPin(a,'D12',0); %//Sets direction of CH A
   writePWMVoltage(a,'D3', 5);  %//Moves CH A
   pause(delaylength);
   
- 
-  writeDigitalPin(a,'D9',1);   %//DISABLE CH A
-  writeDigitalPin(a,'D8',0);   %//ENABLE CH B
+  check_sensor(a);
+  writeDigitalPin(a,'D9', 1);   %//DISABLE CH A
+  writeDigitalPin(a,'D8', 0);   %//ENABLE CH B
   writeDigitalPin(a,'D13',1);  %//Sets direction of CH B
   writePWMVoltage(a,'D11',5);        %//Moves CH B
   pause(delaylength);
-  
-  sensor=readDigitalPin(a,'D5');
-  if sensor ==0
-       break;
-  end
-  end
+ end
   stop_motor(a);
+end
 %   clear(a);
 function stop_motor(a)
-    delaylength = 0.01;
+    delaylength    = 0.01;% in seconds
     writeDigitalPin(a,'D9',1);  %//DISABLE CH A
     writePWMVoltage(a,'D3', 0); %//stop Move CH A
     writeDigitalPin(a,'D8',1);  %//DISABLE CH B
@@ -61,9 +58,9 @@ function stop_motor(a)
     pause(delaylength);
 end
 
-% function check_sensor(a)
-%   sensor=readDigitalPin(a,'D5')
-%   if sensor ==0
-%        break;
-%   end
-% end
+function check_sensor(a)
+  sensor    = readDigitalPin(a,'D5');
+  if sensor ==0
+     stop_motor(a);
+  end
+end
